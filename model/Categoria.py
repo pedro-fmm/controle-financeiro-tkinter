9cea6014564b3d475f0dedb57e7f7e4a638dac0b
nuvme <pedrofmm13@gmail.com>
2023-05-14 15:07:56 -0300

    modelo do objeto categoria

---
+++ model/Categoria.py
@@ -0,0 +1,7 @@
+class Categoria:
+    
+    def __init__(self, id, titulo, is_entrada, is_saida):
+        self.id = id
+        self.titulo = titulo
+        self.is_entrada = is_entrada
+        self.is_saida = is_saida
